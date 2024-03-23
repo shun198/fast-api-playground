238d85e921b81a4f8dfd276a797d1b161f6d6997
shun198 <shunhiroseluvmri@gmail.com>
2024-03-23 16:30:00 +0900

    👍 add healthcheck

--- application/main.py
+++ application/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI Playground",
+    version="0.0.1",
+)
 
-@app.get("/api/health")
+@app.get("/api/health", tags=["health"])
 async def health_check():
     return {"msg": "pass"}
