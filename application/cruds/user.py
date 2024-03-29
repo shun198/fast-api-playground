6664a85acb9f5cfbbb45ce49cafa54c4696a802e
shun198 <shunhiroseluvmri@gmail.com>
2024-03-29 15:40:56 +0900

    👍 update poetry

--- application/cruds/user.py
+++ application/cruds/user.py
@@ -1,7 +1,7 @@
-from sqlalchemy.orm import Session
-
 from models.user import User
+from passlib.context import CryptContext
 from schemas.user import UserCreate
+from sqlalchemy.orm import Session
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 10):
@@ -23,3 +23,27 @@ def create_user(db: Session, user: UserCreate):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+# def get_user(db, username: str):
+#     if username in db:
+#         user_dict = db[username]
+#         return UserInDB(**user_dict)
+
+
+# def authenticate_user(fake_db, username: str, password: str):
+#     user = get_user(fake_db, username)
+#     if not user:
+#         return False
+#     if not verify_password(password, user.hashed_password):
+#         return False
+#     return user
--- application/main.py
+++ application/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-
 from routers import common, user
 from settings.database import init_db
 
