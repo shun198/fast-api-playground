0ab6f71eb8ed8998b0676861dd0c8e13c00073b9
s.hirose <shunichi_hirose@pj100.biz>
2024-09-18 09:14:05 +0900

    👍 add conftest

--- application/tests/conftest.py
+++ application/tests/conftest.py
@@ -1,1 +1,12 @@
 import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+
+
+@pytest.fixture(scope="session")
+def client():
+    """APIのテスト用クライアント"""
+
+    client = TestClient(app)
+    return client
